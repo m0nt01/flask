56c2650669752aa60f952d27c07e2c78562f11ee
Estudante 1 <estudante1@debian.spo.ifsp.edu.br>
2024-06-12 13:56:27 -0300

    first commit

---
+++ jubilant-system_LP3-main/banana/app.py
@@ -0,0 +1,29 @@
+from flask import Flask , render_template
+lista_produtos = [
+        {"nome": "Coca-cola" , "descricao":"Bom" },
+        {"nome": "Pepsi" , "descricao":"Ruim" },
+        {"nome": "Dolly" , "descricao":"Custo beneficios" },
+    ]
+
+app = Flask(__name__)
+
+@app.route("/")
+def home(): 
+    return '<h1>Home</h1>'
+
+@app.route("/contato")
+def contato(): 
+    return '<h1>Contato</h1>'
+
+@app.route("/produtos")
+def lista_produtos():
+    return render_template("produtos.html", produtos = lista_produtos)
+ 
+@app.route("/produtos/home/<nome>")
+def produto(nome):
+    for produto in lista_produtos:
+        if produto("nome") == nome:
+            return f"{produto[nome]},{produto['descricao']}"
+
+
+    return nome 
